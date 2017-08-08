4aa8645fd63a8ce271f39371d4710edef13347f7
scouterryan <scouterryan@gmail.com>
2017-08-08 09:23:35 -0400

    Update hello_ world.py

--- hello_ world.py
+++ hello_ world.py
@@ -1,1 +1,6 @@
-print("hello python world!")
+message= "hello python world!"
+print (message) 
+message= "hello python crash course world"
+print (message)
+      print (mesage)
+# this is the first in a long line of practice messeges with errors#
